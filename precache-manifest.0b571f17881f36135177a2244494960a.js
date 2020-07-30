4786705a01cc1b8ad2e301f687d327eafce38854
pratikwaware <pratikwaware919@gmail.com>
2020-07-30 11:09:00 +0530

    Updates

---
+++ precache-manifest.0b571f17881f36135177a2244494960a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53031f5581754e55fe98ca09d05061c0",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "6dec0b8e3d05d719c01a",
+    "url": "/myapp/static/css/main.5890e42c.chunk.css"
+  },
+  {
+    "revision": "f66634bdd67b7afb4273",
+    "url": "/myapp/static/js/2.ecdec5fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myapp/static/js/2.ecdec5fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dec0b8e3d05d719c01a",
+    "url": "/myapp/static/js/main.7ddb7054.chunk.js"
+  },
+  {
+    "revision": "dfc084a7ee4f2eebf7cd",
+    "url": "/myapp/static/js/runtime-main.5f0af923.js"
+  }
+]);
